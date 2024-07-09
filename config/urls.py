98aad7282b147f8be72b2036e72711c281585772
vitek <rgavih@yandex.ru>
2024-07-09 12:10:47 +0700

    change in urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@
     2. Добавьте URL-адрес в urlpatterns: path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,3 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
 ]
+if settings.DEBUG:  # dz_20.1
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # dz_20.1
