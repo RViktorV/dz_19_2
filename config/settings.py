28967a6c90736fe82ad9d8cd0e70fc271eed2492
vitek <rgavih@yandex.ru>
2024-07-09 12:10:34 +0700

    change in settings.py

--- config/settings.py
+++ config/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', # dz_20.1
+        'NAME': 'dz_20_1', # dz_20.1
+        'USER': 'postgres', # dz_20.1
+        'PASSWORD': '1q2w3e', # dz_20.1
+        'HOST': '127.0.0.1', # dz_20.1
+        'PORT': '5432', # dz_20.1
     }
 }
-
 # Проверка пароля
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -117,3 +120,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/' # dz_20.1
+MEDIA_ROOT = BASE_DIR / 'media' # dz_20.1
