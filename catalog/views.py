771ec3634510187199581184dcd2ae31c5fd905d
vitek <rgavih@yandex.ru>
2024-07-18 11:31:03 +0700

    dz_20_2

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    item_name = Product.objects.all()
+    context = {
+        'items': item_name
+    }
+    return render(request, 'catalog/product_list.html', context)
 
 
 def contacts(request):
@@ -12,3 +18,17 @@ def contacts(request):
         message = request.POST.get('message')
         print((f"Имя-{name},Телефон-{phone},Сообщение-{message}"))
     return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, pk):
+    # Получение объекта товара по его первичному ключу
+    product = get_object_or_404(Product, pk=pk)
+
+    # Формирование контекста для шаблона
+    context = {
+        'item': product
+    }
+
+    # Рендеринг шаблона с переданным контекстом
+    return render(request, 'catalog/product_detail.html', context)
+
