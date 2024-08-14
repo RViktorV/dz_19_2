0b580f3e77f020b3549ef0a3bd77c8fd44814c7c
vitek <rgavih@yandex.ru>
2024-08-15 01:10:09 +0700

    changes 15.08.24 in 01:10

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
+from users.models import Users
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from users.forms import UserRegisterForm
+
+
+class UserCreateView(CreateView):
+    model = Users
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/user_form.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+
+
+
+
+
+
 from django.shortcuts import render
 
 # Create your views here.
