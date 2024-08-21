105c88ea667d8ab114d90e9abb052bb82e80c5dd
RViktorV <rgavih@yandex.ru>
2024-08-21 11:59:49 +0700

    changes from 21.08.24 12:00

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Group
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -54,32 +55,20 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('catalog:home')
     template_name = 'catalog/product_form.html'
 
+    from django.contrib.auth.models import Group
     def get_context_data(self, **kwargs):
-        """
-        Расширьте данные контекста с помощью набора форм для управления связанными экземплярами «Версии».
-
-        Этот метод создает встроенный набор форм для модели «Версия», связанной с «Продуктом».
-        пример. Набор форм включается в контекстные данные, которые передаются в шаблон.
-        для рендеринга.
-
-        Если метод запроса — POST, он привязывает набор форм к данным POST для обработки формы.
-        подчинение. В противном случае он инициализирует набор форм существующими экземплярами «Версия».
-        связанный с текущим объектом `Product`.
-
-        Аргументы:
-            **kwargs: в метод передаются дополнительные контекстные данные.
-
-        Возврат:
-            dict: обновленные данные контекста, включая набор форм для управления экземплярами «Версия».
-        """
-
-        context_data = super().get_context_data(**kwargs)                                           # Получить существующие данные контекста из родительского класса
-        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)     # Создайте встроенную фабрику наборов форм для модели «Версия», связанной с моделью «Продукт».
-        if self.request.method == 'POST':                                                       # Проверьте, является ли метод запроса POST (отправка формы)
-            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)   # Если POST, привяжите набор форм к данным POST.
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
         else:
-            context_data['formset'] = VersionFormset(instance=self.object)                      # Если не POST, инициализируйте набор форм существующими экземплярами Version.
-        return context_data                                                                     # Вернуть обновленные данные контекста
+            # Проверяем, входит ли пользователь в группу Moderators
+            moderators_group = Group.objects.get(name='Moderators')
+            # Если пользователь не в группе Moderators, не включаем форму для версий
+            if not moderators_group in self.request.user.groups.all():
+                context_data['formset'] = VersionFormset(instance=self.object)
+        # context_data['additional_info'] = 'This is extra data'
+        return context_data
 
     def form_valid(self, form):
         context_data = self.get_context_data()
@@ -87,8 +76,8 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         if formset.is_valid():
             active_versions_count = sum(1 for form in formset if form.cleaned_data.get('is_current'))
             if active_versions_count > 1:
-                formset.non_form_errors = ['Вы можете выбрать только одну активную версию для продукта.']
-                return self.form_invalid(form)  # Возвращаем ошибку формы
+                form.add_error(None, 'Вы можете выбрать только одну активную версию для продукта.')  # Добавляем ошибку
+                return self.form_invalid(form)
             formset.save()
         return super().form_valid(form)
 
